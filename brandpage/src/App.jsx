b39c27a8d226df08955b6d83f86daa7e1cf62eef
AdarshYadav-dev <adarsh98215@gmail.com>
2025-09-15 00:57:25 +0530

    Initial commit - responsive landing page

---
+++ brandpage/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Brandpage from './Components/Brandpage'
+import Hero from './Components/Hero'
+import './App.scss';
+
+function App() {
+
+
+  return (
+    <>
+   <Brandpage />
+   <Hero />
+   </>
+  )
+}
+
+export default App
